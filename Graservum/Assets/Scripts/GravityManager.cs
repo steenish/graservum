776eb56ba492268cc61eeaa8bbae35f3fa40fa85
Geodrop Studios <geodropstudios@gmail.com>
2018-12-31 17:41:01 +0100

    Started developing gravity
    Created script and invokerepeating, not much more.

---
+++ Graservum/Assets/Scripts/GravityManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityManager : MonoBehaviour {
+
+    GameObject[] gravityObjects;
+
+    void Start() {
+        InvokeRepeating(updateList(), 0, 1);
+    }
+
+    void Update() {
+        
+    }
+
+    void updateList() {
+        gravityObjects = GameObject.FindGameObjectsWithTag("GravityObject");
+    }
+}
