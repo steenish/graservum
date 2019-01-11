0f37ced22341fac2965db54051b45829903de8a6
Geodrop Studios <geodropstudios@gmail.com>
2019-01-11 20:59:29 +0100

    Tried implementing gravity
    The gravity manager script is taking shape, but despite everything seeming like it should work, none of the objects seem affected by the forces.

--- Graservum/Assets/Scripts/GravityManager.cs
+++ Graservum/Assets/Scripts/GravityManager.cs
@@ -4,17 +4,50 @@ using UnityEngine;
 
 public class GravityManager : MonoBehaviour {
 
+    public float gravityModifier = 1;
+
+    const double GRAVITY_CONSTANT = 0.0000000000667408;
     GameObject[] gravityObjects;
 
     void Start() {
-        InvokeRepeating(updateList(), 0, 1);
+        updateList();
+        InvokeRepeating("updateList", 1.0f, 1.0f);
     }
 
-    void Update() {
-        
+    void FixedUpdate() {
+        exertGravity();
     }
 
     void updateList() {
-        gravityObjects = GameObject.FindGameObjectsWithTag("GravityObject");
+        gravityObjects = HelperFunctions.FindGameObjectsOnLayer("GravityObjects");
+    }
+
+    void exertGravity() {
+        for (int i = 0; i < gravityObjects.Length; i++) {
+            for (int j = i + 1; j < gravityObjects.Length; j++) {
+                if (gravityObjects[i] != null && gravityObjects[j] != null && i != j) {
+                    GameObject object1 = gravityObjects[i];
+                    GameObject object2 = gravityObjects[j];
+                    Vector3 position1 = object1.transform.position;
+                    Vector3 position2 = object2.transform.position;
+
+                    float magnitude = calculateGravityNewton(object1.GetComponent<Rigidbody>().mass, object2.GetComponent<Rigidbody>().mass, Vector3.Distance(position1, position2)) * gravityModifier * Time.fixedDeltaTime;
+
+                    Vector3 direction = (position2 - position1).normalized; // From position1 to position2.
+                    Debug.Log("Direction from object1 to object 2: " + direction);
+
+                    object1.GetComponent<Rigidbody>().AddForce(magnitude * direction); // Apply gravitational force on object1 towards object2.
+                    Debug.Log("Force on object1: " + magnitude * direction);
+
+                    direction = -direction; // Invert direction of force.
+                    object1.GetComponent<Rigidbody>().AddForce(magnitude * direction); // Apply gravitational force on object2 towards object1.
+                    Debug.Log("Force on object2: " + magnitude * direction);
+                }
+            }
+        }
+    }
+
+    float calculateGravityNewton(float m1, float m2, float r) {
+        return (float) GRAVITY_CONSTANT * (m1 * m2) / (r * r);
     }
 }
---
+++ Graservum/Assets/Scripts/HelperFunctions.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelperFunctions : MonoBehaviour {
+
+    // Returns an array of GameObjects that are on the layer which name was specified by the argument. Returns null if no such layer was found.
+    public static GameObject[] FindGameObjectsOnLayer(string layerName) {
+        int layer = LayerMask.NameToLayer(layerName);
+
+        if (layer > -1) {
+            GameObject[] candidates = GetAllGameObjectsInScene();
+            List<GameObject> result = new List<GameObject>();
+
+            foreach (GameObject candidate in candidates) {
+                if (candidate.layer == layer) {
+                    result.Add(candidate);
+                }
+            }
+
+            return result.ToArray();
+        }
+
+        return null;
+    }
+
+    // Returns an array of all GameObjects that are active in the Scene Hierarchy.
+    public static GameObject[] GetAllGameObjectsInScene() {
+        GameObject[] candidates = FindObjectsOfType<GameObject>();
+        List<GameObject> result = new List<GameObject>();
+
+        foreach (GameObject candidate in candidates) {
+            if (candidate.activeInHierarchy) {
+                result.Add(candidate);
+            }
+        }
+
+        return result.ToArray();
+    }
+}
