53874ffbee7e550164260a51b1e2410fadd00701
Anders Steen <anders.steen98@gmail.com>
2019-01-27 19:04:14 +0100

    Started work on ConservationTests
    Created the scene and some basic conservation of momentum behavioral code in the MomentumController script.

---
+++ Graservum/Assets/Scripts/MomentumController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class MomentumController : MonoBehaviour {
+
+    Rigidbody _rigidbody;
+
+    void Start()
+    {
+        _rigidbody = gameObject.GetComponent<Rigidbody>();
+    }
+
+    void OnTriggerEnter(Collider other) {
+        Rigidbody otherRigidbody = other.gameObject.GetComponent<Rigidbody>();
+
+        // Check if the other collider is a gravity object.
+        if (other.gameObject.layer == LayerMask.NameToLayer("GravityObjects")) {
+
+            // Check if the other object has a greater mass.
+            if (otherRigidbody.mass < _rigidbody.mass) {
+
+                // Add together masses and velocities and destroy the other object.
+                _rigidbody.mass += otherRigidbody.mass;
+                _rigidbody.velocity += otherRigidbody.velocity;
+                Destroy(other.gameObject);
+            } else if (otherRigidbody.mass == _rigidbody.mass) { // Corner case when the masses are the same.
+                // Do something.
+            }
+        }
+    }
+}
