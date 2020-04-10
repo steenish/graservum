00bb647355f4c8a69c8b39d072614c03164e568d
Geodrop Studios <geodropstudios@gmail.com>
2020-04-10 14:40:21 +0200

    complete camera control tests

---
+++ Graservum/Assets/Scripts/CameraController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    [SerializeField]
+    [Range(0.0f, 10.0f)]
+    private float zOffset = 10.0f;
+
+    void Update() {
+        transform.position = transform.parent.position + new Vector3(0.0f, 0.0f, -zOffset);
+        transform.rotation = Quaternion.identity;
+    }
+}
