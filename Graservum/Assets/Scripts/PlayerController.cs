fc59d0c1f5186004ff1c725a727f351f4541ec84
Geodrop Studios <geodropstudios@gmail.com>
2020-04-14 01:37:02 +0200

    continue emission tests

--- Graservum/Assets/Scripts/PlayerController.cs
+++ Graservum/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,8 @@ public class PlayerController : MonoBehaviour {
     [SerializeField]
     private float emissionSpeed = 10.0f;
     [SerializeField]
+    private float maxEmittedMass = 10.0f;
+    [SerializeField]
     private Transform childCollectionTransform;
     [SerializeField]
     private LineRenderer line;
@@ -17,30 +19,27 @@ public class PlayerController : MonoBehaviour {
     private float timeToMaxValue = 2.0f;
     [SerializeField]
     private Slider massSlider;
+    [SerializeField]
+    private GameObject emittedObjectPrefab;
 
     private bool _instantiateOnNextFixedUpdate;
-    private float _timeClicked;
     private float _accumulatedTime;
+    private float _massSliderProgress;
     private Rigidbody _rigidbody;
-    private Vector3 _emissionVelocityDirection;
 
     void Start() {
         _rigidbody = GetComponent<Rigidbody>();
-        _emissionVelocityDirection = Vector3.Normalize(-transform.forward);
     }
 
     void Update() {
         // Get mouse position to target.
-        Vector3 targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        targetPos.z = transform.position.z;
-        targetPos = 100 * transform.InverseTransformPoint(targetPos);
+        Vector3 targetPosition = 100 * GetMousePosTargetDirection();
 
         // Set line direction.
-        line.SetPosition(1, targetPos);
+        line.SetPosition(1, targetPosition);
 
         // Check for new clicks, start slider timer.
         if (Input.GetMouseButtonDown(0)) {
-            _timeClicked = Time.time;
             _accumulatedTime = 0.0f;
         }
 
@@ -48,40 +47,49 @@ public class PlayerController : MonoBehaviour {
         if (Input.GetMouseButton(0)) {
             _accumulatedTime += Time.deltaTime;
             float progress = _accumulatedTime / timeToMaxValue * massSlider.maxValue;
-            Debug.Log("progress = " + progress);
             massSlider.value = Mathf.Clamp(progress, massSlider.minValue, massSlider.maxValue);
         }
 
         // Check for mouse button release, set emission flag and reset slider.
         if (Input.GetMouseButtonUp(0)) {
-            _instantiateOnNextFixedUpdate = true;
+            _massSliderProgress = massSlider.value / (massSlider.maxValue - massSlider.minValue);
             massSlider.value = massSlider.minValue;
+            _instantiateOnNextFixedUpdate = true;
         }
     }
 
     void FixedUpdate() {
-        //if (_instantiateOnNextFixedUpdate) {
-        //    // Calculate mass.
-        //    float newMass = _rigidbody.mass - emittedMass;
-        //    Debug.Log("newMass = " + newMass);
+        if (_instantiateOnNextFixedUpdate) {
+            // Calculate mass.
+            float emittedMass = _massSliderProgress * maxEmittedMass;
+            float newMass = _rigidbody.mass - emittedMass;
+            Debug.Log("newMass = " + newMass);
+            Debug.Log("emittedMass = " + emittedMass);
 
-        //    // Calculate velocity.
-        //    Vector3 newVelocity = Time.fixedDeltaTime * (_rigidbody.mass * _rigidbody.velocity - emittedMass * _emissionVelocityDirection * emissionSpeed) / newMass;
+            // Get mouse position to target.
+            Vector3 targetDirection = GetMousePosTargetDirection();
 
-        //    // Instantiate child.
-        //    GameObject childObject = Instantiate(gameObject, _emissionVelocityDirection * transform.localScale.x, transform.rotation, childCollectionTransform);
-        //    Destroy(childObject.GetComponent<PlayerController>());
-        //    Destroy(childObject.GetComponent<MomentumController>());
+            // Calculate velocity.
+            Vector3 newVelocity = Time.fixedDeltaTime * (_rigidbody.mass * _rigidbody.velocity - emittedMass * targetDirection * emissionSpeed) / newMass;
 
-        //    // Update mass and velocities.
-        //    Rigidbody childRigidbody = childObject.GetComponent<Rigidbody>();
-        //    childRigidbody.mass = emittedMass;
-        //    childRigidbody.velocity = _emissionVelocityDirection * emissionSpeed;
+            // Instantiate child.
+            GameObject childObject = Instantiate(emittedObjectPrefab, transform.position + targetDirection * transform.localScale.x, transform.rotation, childCollectionTransform);
 
-        //    _rigidbody.mass = newMass;
-        //    _rigidbody.velocity = newVelocity;
+            // Update mass and velocities.
+            Rigidbody childRigidbody = childObject.GetComponent<Rigidbody>();
+            childRigidbody.mass = emittedMass;
+            childRigidbody.velocity = targetDirection * emissionSpeed;
 
-        //    _instantiateOnNextFixedUpdate = false;
-        //}
+            _rigidbody.mass = newMass;
+            _rigidbody.velocity = newVelocity;
+
+            _instantiateOnNextFixedUpdate = false;
+        }
+    }
+
+    private Vector3 GetMousePosTargetDirection() {
+        Vector3 targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        targetPos.z = transform.position.z;
+        return Vector3.Normalize(transform.InverseTransformPoint(targetPos));
     }
 }
