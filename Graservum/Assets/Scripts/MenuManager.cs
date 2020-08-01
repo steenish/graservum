f860589cb1de28cbdb7ea57feaba1af40562bd32
Anders Steen <anders.steen98@gmail.com>
2020-08-01 02:50:51 +0200

    start creating main menu

---
+++ Graservum/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+#pragma warning disable
+    [SerializeField]
+    private Animator animator;
+#pragma warning restore
+
+    public void StartGame() {
+        SceneManager.LoadSceneAsync("MainGameScene");
+    }
+
+    public void ShowMainMenu() {
+        animator.SetTrigger("ShowMain");
+    }
+
+    public void ShowInstructionsMenu() {
+        animator.SetTrigger("ShowInstructions");
+    }
+
+    public void ShowSettingsMenu() {
+        animator.SetTrigger("ShowSettings");
+    }
+}
--- Graservum/Assets/Scripts/VelocityInitializer.cs
+++ Graservum/Assets/Scripts/VelocityInitializer.cs
@@ -6,10 +6,12 @@ public class VelocityInitializer : MonoBehaviour {
 
     public GameObject[] objects;
     public Vector3[] velocities;
+    public Vector3[] angularVelocities;
     
     void Start() {
         for (int i = 0; i < objects.Length; i++) {
             objects[i].GetComponent<Rigidbody>().velocity = velocities[i];
+            objects[i].GetComponent<Rigidbody>().angularVelocity = angularVelocities[i];
         }
     }
 }
