43c8bcd4d10aed2ee138d7616a7d03474977a1f2
Anders Steen <anders.steen98@gmail.com>
2020-08-23 21:18:04 +0200

    implement player warning sprite

---
+++ Graservum/Assets/Scripts/PlayerWarningSpriteController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWarningSpriteController : MonoBehaviour {
+
+#pragma warning disable
+    [SerializeField]
+    private PlayerInput playerInput;
+    [SerializeField]
+    private Rigidbody playerAsteroidRigidbody;
+    [SerializeField]
+    private SpriteRenderer warningSpriteRenderer;
+    [SerializeField]
+    private float massFromMaxSpriteEnabler;
+#pragma warning restore
+
+    void Start() {
+        
+    }
+
+    void Update() {
+        if (!warningSpriteRenderer.enabled && playerAsteroidRigidbody.mass > playerInput.maxAsteroidMass - massFromMaxSpriteEnabler) {
+            warningSpriteRenderer.enabled = true;
+        }
+
+        if (warningSpriteRenderer.enabled && playerAsteroidRigidbody.mass <= playerInput.maxAsteroidMass - massFromMaxSpriteEnabler) {
+            warningSpriteRenderer.enabled = false;
+        }
+    }
+}
